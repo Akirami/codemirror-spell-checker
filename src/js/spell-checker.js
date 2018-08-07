b5b29c70629e79d80cf83b62812a1fc85e161626
Taylor Yelverton <tny5357@gmail.com>
2018-08-07 17:10:34 -0500

    Allow multiple dictionaries.

--- src/js/spell-checker.js
+++ src/js/spell-checker.js
@@ -2,119 +2,138 @@
 "use strict";
 
 var Typo = window.Typo;
+var $ = window.$;
 
 // Check if the string is in alphabets
 // Borrowed from: https://stackoverflow.com/a/17056233/1217590
 function isAlpha(string) {
-	var patt = /^[a-zA-Z\u00C6\u00D0\u018E\u018F\u0190\u0194\u0132\u014A\u0152\u1E9E\u00DE\u01F7\u021C\u00E6\u00F0\u01DD\u0259\u025B\u0263\u0133\u014B\u0153\u0138\u017F\u00DF\u00FE\u01BF\u021D\u0104\u0181\u00C7\u0110\u018A\u0118\u0126\u012E\u0198\u0141\u00D8\u01A0\u015E\u0218\u0162\u021A\u0166\u0172\u01AFY\u0328\u01B3\u0105\u0253\u00E7\u0111\u0257\u0119\u0127\u012F\u0199\u0142\u00F8\u01A1\u015F\u0219\u0163\u021B\u0167\u0173\u01B0y\u0328\u01B4\u00C1\u00C0\u00C2\u00C4\u01CD\u0102\u0100\u00C3\u00C5\u01FA\u0104\u00C6\u01FC\u01E2\u0181\u0106\u010A\u0108\u010C\u00C7\u010E\u1E0C\u0110\u018A\u00D0\u00C9\u00C8\u0116\u00CA\u00CB\u011A\u0114\u0112\u0118\u1EB8\u018E\u018F\u0190\u0120\u011C\u01E6\u011E\u0122\u0194\u00E1\u00E0\u00E2\u00E4\u01CE\u0103\u0101\u00E3\u00E5\u01FB\u0105\u00E6\u01FD\u01E3\u0253\u0107\u010B\u0109\u010D\u00E7\u010F\u1E0D\u0111\u0257\u00F0\u00E9\u00E8\u0117\u00EA\u00EB\u011B\u0115\u0113\u0119\u1EB9\u01DD\u0259\u025B\u0121\u011D\u01E7\u011F\u0123\u0263\u0124\u1E24\u0126I\u00CD\u00CC\u0130\u00CE\u00CF\u01CF\u012C\u012A\u0128\u012E\u1ECA\u0132\u0134\u0136\u0198\u0139\u013B\u0141\u013D\u013F\u02BCN\u0143N\u0308\u0147\u00D1\u0145\u014A\u00D3\u00D2\u00D4\u00D6\u01D1\u014E\u014C\u00D5\u0150\u1ECC\u00D8\u01FE\u01A0\u0152\u0125\u1E25\u0127\u0131\u00ED\u00ECi\u00EE\u00EF\u01D0\u012D\u012B\u0129\u012F\u1ECB\u0133\u0135\u0137\u0199\u0138\u013A\u013C\u0142\u013E\u0140\u0149\u0144n\u0308\u0148\u00F1\u0146\u014B\u00F3\u00F2\u00F4\u00F6\u01D2\u014F\u014D\u00F5\u0151\u1ECD\u00F8\u01FF\u01A1\u0153\u0154\u0158\u0156\u015A\u015C\u0160\u015E\u0218\u1E62\u1E9E\u0164\u0162\u1E6C\u0166\u00DE\u00DA\u00D9\u00DB\u00DC\u01D3\u016C\u016A\u0168\u0170\u016E\u0172\u1EE4\u01AF\u1E82\u1E80\u0174\u1E84\u01F7\u00DD\u1EF2\u0176\u0178\u0232\u1EF8\u01B3\u0179\u017B\u017D\u1E92\u0155\u0159\u0157\u017F\u015B\u015D\u0161\u015F\u0219\u1E63\u00DF\u0165\u0163\u1E6D\u0167\u00FE\u00FA\u00F9\u00FB\u00FC\u01D4\u016D\u016B\u0169\u0171\u016F\u0173\u1EE5\u01B0\u1E83\u1E81\u0175\u1E85\u01BF\u00FD\u1EF3\u0177\u00FF\u0233\u1EF9\u01B4\u017A\u017C\u017E\u1E93]+$/;
-	return patt.test(string);
+  var patt = /^[a-zA-Z\u00C6\u00D0\u018E\u018F\u0190\u0194\u0132\u014A\u0152\u1E9E\u00DE\u01F7\u021C\u00E6\u00F0\u01DD\u0259\u025B\u0263\u0133\u014B\u0153\u0138\u017F\u00DF\u00FE\u01BF\u021D\u0104\u0181\u00C7\u0110\u018A\u0118\u0126\u012E\u0198\u0141\u00D8\u01A0\u015E\u0218\u0162\u021A\u0166\u0172\u01AFY\u0328\u01B3\u0105\u0253\u00E7\u0111\u0257\u0119\u0127\u012F\u0199\u0142\u00F8\u01A1\u015F\u0219\u0163\u021B\u0167\u0173\u01B0y\u0328\u01B4\u00C1\u00C0\u00C2\u00C4\u01CD\u0102\u0100\u00C3\u00C5\u01FA\u0104\u00C6\u01FC\u01E2\u0181\u0106\u010A\u0108\u010C\u00C7\u010E\u1E0C\u0110\u018A\u00D0\u00C9\u00C8\u0116\u00CA\u00CB\u011A\u0114\u0112\u0118\u1EB8\u018E\u018F\u0190\u0120\u011C\u01E6\u011E\u0122\u0194\u00E1\u00E0\u00E2\u00E4\u01CE\u0103\u0101\u00E3\u00E5\u01FB\u0105\u00E6\u01FD\u01E3\u0253\u0107\u010B\u0109\u010D\u00E7\u010F\u1E0D\u0111\u0257\u00F0\u00E9\u00E8\u0117\u00EA\u00EB\u011B\u0115\u0113\u0119\u1EB9\u01DD\u0259\u025B\u0121\u011D\u01E7\u011F\u0123\u0263\u0124\u1E24\u0126I\u00CD\u00CC\u0130\u00CE\u00CF\u01CF\u012C\u012A\u0128\u012E\u1ECA\u0132\u0134\u0136\u0198\u0139\u013B\u0141\u013D\u013F\u02BCN\u0143N\u0308\u0147\u00D1\u0145\u014A\u00D3\u00D2\u00D4\u00D6\u01D1\u014E\u014C\u00D5\u0150\u1ECC\u00D8\u01FE\u01A0\u0152\u0125\u1E25\u0127\u0131\u00ED\u00ECi\u00EE\u00EF\u01D0\u012D\u012B\u0129\u012F\u1ECB\u0133\u0135\u0137\u0199\u0138\u013A\u013C\u0142\u013E\u0140\u0149\u0144n\u0308\u0148\u00F1\u0146\u014B\u00F3\u00F2\u00F4\u00F6\u01D2\u014F\u014D\u00F5\u0151\u1ECD\u00F8\u01FF\u01A1\u0153\u0154\u0158\u0156\u015A\u015C\u0160\u015E\u0218\u1E62\u1E9E\u0164\u0162\u1E6C\u0166\u00DE\u00DA\u00D9\u00DB\u00DC\u01D3\u016C\u016A\u0168\u0170\u016E\u0172\u1EE4\u01AF\u1E82\u1E80\u0174\u1E84\u01F7\u00DD\u1EF2\u0176\u0178\u0232\u1EF8\u01B3\u0179\u017B\u017D\u1E92\u0155\u0159\u0157\u017F\u015B\u015D\u0161\u015F\u0219\u1E63\u00DF\u0165\u0163\u1E6D\u0167\u00FE\u00FA\u00F9\u00FB\u00FC\u01D4\u016D\u016B\u0169\u0171\u016F\u0173\u1EE5\u01B0\u1E83\u1E81\u0175\u1E85\u01BF\u00FD\u1EF3\u0177\u00FF\u0233\u1EF9\u01B4\u017A\u017C\u017E\u1E93]+$/;
+  return patt.test(string);
 }
 
 // Create function
 function CodeMirrorSpellChecker(options) {
-	// Initialize
-	options = options || {};
-	var language = options.language || "en_US";
-	var remoteLanguageUrl = options.remoteLanguageUrl || "https://cdn.jsdelivr.net/codemirror.spell-checker/latest/";
+  // Initialize
+  options = options || {};
 
+  // Verify
+  if(typeof options.codeMirrorInstance !== "function" || typeof options.codeMirrorInstance.defineMode !== "function") {
+    console.error(new Error("CodeMirror Spell Checker: You must provide an instance of CodeMirror via the option `codeMirrorInstance`"));
+    return;
+  }
 
-	// Verify
-	if(typeof options.codeMirrorInstance !== "function" || typeof options.codeMirrorInstance.defineMode !== "function") {
-		console.log("CodeMirror Spell Checker: You must provide an instance of CodeMirror via the option `codeMirrorInstance`");
-		return;
-	}
+  var language = options.language || "en_US";
+  var dictionaries = CodeMirrorSpellChecker.dictionaries = options.dictionaries || [
+    { url: "https://cdn.jsdelivr.net/codemirror.spell-checker/latest/" }
+  ];
 
+  var loadRemoteDic = function (dictionary, type) {
+    return function (success) {
+      return function(data) {
+        if (success) {
+          dictionary[type] = data;
+        } else {
+          dictionary[type] = '';
+        }
+        for (var i = 0; i < dictionaries.length; i++) {
+          if ((typeof dictionaries[i].aff != 'string') || (typeof dictionaries[i].dic != 'string')) return
+        }
+        initializeTypo();
+      };
+    };
+  };
 
-	// Because some browsers don't support this functionality yet
-	if(!String.prototype.includes) {
-		String.prototype.includes = function() {
-			"use strict";
-			return String.prototype.indexOf.apply(this, arguments) !== -1;
-		};
-	}
+  var initializeTypo = function () {
+    CodeMirrorSpellChecker.aff = '';
+    CodeMirrorSpellChecker.dic = '';
+    for (var i = 0; i < dictionaries.length; i++) {
+      dictionary = dictionaries[i];
+      CodeMirrorSpellChecker.aff += "\n" + dictionary.aff;
+      CodeMirrorSpellChecker.dic += "\n" + dictionary.dic;
+    }
+    CodeMirrorSpellChecker.typo = new Typo(
+      language,
+      CodeMirrorSpellChecker.aff,
+      CodeMirrorSpellChecker.dic,
+      {
+        platform: "any"
+      }
+    );
+  }
 
-	var loadRemoteDic = function(type) {
-		return function() {
-			if(this.readyState === 4 && this.status === 200) {
-				CodeMirrorSpellChecker[type] = this.responseText;
-				CodeMirrorSpellChecker.num_loaded++;
+  for (var i = 0; i < dictionaries.length; i++) {
+    var dictionary = dictionaries[i];
+    if (!dictionary.hasOwnProperty('url')) {
+      console.error(new Error('CodeMirrorSpellChecker requested dictionary is missing url'), dictionary);
+      return;
+    }
+    if (!dictionary.hasOwnProperty('aff')) dictionary.aff = true;
+    if (!dictionary.aff) dictionary.aff = '';
+    if (!dictionary.hasOwnProperty('dic')) dictionary.dic = true;
+    if (!dictionary.dic) dictionary.dic = '';
+    if (dictionary.url) {
+      if (dictionary.aff === true) {
+        var loader = loadRemoteDic(dictionary, 'aff');
+        $.ajax({
+          url: dictionary.url + language + '.aff',
+          success: loader(true),
+          error: loader(false),
+        });
+      }
+      if (dictionary.dic === true) {
+        var loader = loadRemoteDic(dictionary, 'dic');
+        $.ajax({
+          url: dictionary.url + language + '.dic',
+          success: loader(true),
+          error: loader(false),
+        });
+      }
+    }
+  }
 
-				if(CodeMirrorSpellChecker.num_loaded == 2) {
-					CodeMirrorSpellChecker.typo = new Typo(language, CodeMirrorSpellChecker.aff_data, CodeMirrorSpellChecker.dic_data, {
-						platform: "any"
-					});
-				}
-			}
-		};
-	};
+  // Define the new mode
+  options.codeMirrorInstance.defineMode("spell-checker", function(config) {
 
-	var loadRemote = function() {
-		// Load AFF/DIC data
-		if(!CodeMirrorSpellChecker.aff_loading) {
-			CodeMirrorSpellChecker.aff_loading = true;
-			var xhr_aff = new XMLHttpRequest();
-			xhr_aff.open("GET", remoteLanguageUrl + language + ".aff", true);
-			xhr_aff.onload = loadRemoteDic("aff_data");
-			xhr_aff.send(null);
-		}
+    // Create the overlay and such
+    var overlay = {
+      token: function(stream) {
+        var ch = stream.peek();
+        var word = "";
 
-		if(!CodeMirrorSpellChecker.dic_loading) {
-			CodeMirrorSpellChecker.dic_loading = true;
-			var xhr_dic = new XMLHttpRequest();
-			xhr_dic.open("GET", remoteLanguageUrl + language + ".dic", true);
-			xhr_dic.onload = loadRemoteDic("dic_data");
-			xhr_dic.send(null);
-		}
-	};
+        // var isCodeBlock = stream.lineOracle.state.base.overlay.codeBlock;
+        // if(options.ignoreCodeBlocks && isCodeBlock) {
+        //   stream.next();
+        //   return null;
+        // }
 
-	// Define the new mode
-	options.codeMirrorInstance.defineMode("spell-checker", function(config) {
+        if(!isAlpha(ch)) {
+          stream.next();
+          return null;
+        }
 
-		loadRemote();
+        while((ch = stream.peek()) != null && isAlpha(ch)) {
+          word += ch;
+          stream.next();
+        }
 
-		// Create the overlay and such
-		var overlay = {
-			token: function(stream) {
-				var ch = stream.peek();
-				var word = "";
+        if(CodeMirrorSpellChecker.typo && !CodeMirrorSpellChecker.typo.check(word))
+          return "spell-error"; // CSS class: cm-spell-error
 
-				// var isCodeBlock = stream.lineOracle.state.base.overlay.codeBlock;
-				// if(options.ignoreCodeBlocks && isCodeBlock) {
-				// 	stream.next();
-				// 	return null;
-				// }
+        return null;
+      }
+    };
 
-				if(!isAlpha(ch)) {
-					stream.next();
-					return null;
-				}
+    var mode = options.codeMirrorInstance.getMode(
+      config, config.backdrop || "text/plain"
+    );
 
-				while((ch = stream.peek()) != null && isAlpha(ch)) {
-					word += ch;
-					stream.next();
-				}
-
-				if(CodeMirrorSpellChecker.typo && !CodeMirrorSpellChecker.typo.check(word))
-					return "spell-error"; // CSS class: cm-spell-error
-
-				return null;
-			}
-		};
-
-		var mode = options.codeMirrorInstance.getMode(
-			config, config.backdrop || "text/plain"
-		);
-
-		return options.codeMirrorInstance.overlayMode(mode, overlay, true);
-	});
+    return options.codeMirrorInstance.overlayMode(mode, overlay, true);
+  });
 }
 
 
 // Initialize data globally to reduce memory consumption
-CodeMirrorSpellChecker.num_loaded = 0;
-CodeMirrorSpellChecker.aff_loading = false;
-CodeMirrorSpellChecker.dic_loading = false;
 CodeMirrorSpellChecker.aff_data = "";
 CodeMirrorSpellChecker.dic_data = "";
 CodeMirrorSpellChecker.typo;
-CodeMirrorSpellChecker.languages = [];
+CodeMirrorSpellChecker.dictionaries = {};
